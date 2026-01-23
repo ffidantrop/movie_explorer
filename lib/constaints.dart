34cbd33786f4b9a318d1de1ccd751f721343a942
ffidantrop <bogdasyk26@gmail.com>
2026-01-23 05:27:01 +0500

    Update constaints.dart

--- lib/constaints.dart
+++ lib/constaints.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/material.dart';
+
 const cAppBar = 'AF4425';
 const cBackGround = '662E1C';
 const cLightSearchBG = 'EBDCB2';
 const cButtonsBG = 'C9A66B';
+
+final myListViewTextStyle = TextStyle(
+  color: Color.fromARGB(255, 235, 220, 178),
+);
